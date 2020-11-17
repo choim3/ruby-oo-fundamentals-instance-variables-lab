ff15a94cc6e61f98711e1e92b5ec38f4d10ff2fc
choim3 <choim3@spu.edu>
2020-11-17 10:56:11 -0800

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog
+    #define name method which takes the name of a dog and has it equal a local variable
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+    #defines a method in which it returns the dogs name
+    def name
+        @this_dogs_name
+    end
+end
+
+
